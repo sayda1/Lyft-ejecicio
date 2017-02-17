305dc1d846069cfbe7d89baf6846e3fd8cefa06d
sayda <sayda>
2017-02-17 11:51:54 -0500

    iconos de ubicacion y mapa

--- js/app.js
+++ js/app.js
@@ -1,28 +1,39 @@
-function init ()
-{
-    
-    
-}
+
 var map;
-function initMap() 
-{
-    var mapConfig={
-        center:{
-            lat: -16.24573891999999998, lng: -71.53151308
-        },
-        zoom:10
-    };
+function initMap() {
     
-    map = new google.maps.Map(document.getElementById('map'));
-   var miLati1={ lat: -16.24573891999999998, lng: -71.53151308};
-   var miLati={ lat: 16.24891999999998, lng: -71.5311308};
-   var miLati={ lat: 16.245799999998, lng: -71.531308}
-  
-   var marker = new google.maps.Marker({
-    position: miLati1,
-    map: map,
-    title: 'Helloaki stoy!',
-    icon:'img/paloma2.jpg'
-  }); 
+    var ubicacion1={ lat: -16.457389199999998, lng: -71.5315308};
+    var ubicacion2={ lat: -16.45738919999998, lng: -71.537808};
+    var ubicacion3={ lat: -16.45738919999598, lng: -71.525728};
+
+    map = new google.maps.Map(document.getElementById('map'),{center:ubicacion1,zoom: 14 });
+    
+    //marcador de ubicacion uno 
+    var marcador1 =new google.maps.Marker({ 
+        position:ubicacion1, 
+        map: map, 
+        title:'Hola peru.!!',     
+        icon:'img/peru.png'}
+    ); 
+    
+  //console.log(marcador1);
+    
+    //marcador de ubicaion dos
+    var marcador2 =new google.maps.Marker({ 
+        position:ubicacion2,
+        map: map,
+        title: ' estoy aqui.!',
+        icon:'img/carito.jpg'
+    });
+    //console.log(marcador2);
+    
+    //marcador de ubicaion tres
+    var marcador3 =new google.maps.Marker({ 
+        position:ubicacion3, 
+        map: map,title: 'hola..si!!!', 
+        icon:'img/persona.jpg'
+    });
+    //console.log(marcador3);*/
 }
+
 
